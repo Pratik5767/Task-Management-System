32fc805067adee797bb5bb19114f1e0ab4552f39
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-03-28 12:35:59 +0530

    setup angular, springboot, created user entity, enum and userRepository

---
+++ backend/src/test/java/com/project/Task_Management_Backend/TaskManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Task_Management_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
