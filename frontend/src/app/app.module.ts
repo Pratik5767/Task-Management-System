fe2f0ddec78b10cd31a4284a19fc0c62f418c1be
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-03-31 12:46:31 +0530

    created login and signup ui in angular

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './auth/components/login/login.component';
 import { SignupComponent } from './auth/components/signup/signup.component';
 import { DemoAngularMaterialModule } from './DemoAngularMaterialModule';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import { DemoAngularMaterialModule } from './DemoAngularMaterialModule';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    DemoAngularMaterialModule
+    DemoAngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
