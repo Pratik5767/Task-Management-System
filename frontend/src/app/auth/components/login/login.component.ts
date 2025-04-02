c7d46cdbb030a55925f9cc7612ee6a6e029d2e57
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-04-02 13:15:34 +0530

    Implemented role based routing for admin and customer

--- frontend/src/app/auth/components/login/login.component.ts
+++ frontend/src/app/auth/components/login/login.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthService } from '../../services/auth/auth.service';
+import { StorageService } from '../../services/storage/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent {
 
     loginForm!: FormGroup;
@@ -16,7 +19,8 @@ export class LoginComponent {
     constructor(
         private fb: FormBuilder,
         private authService: AuthService,
-        private snakBar: MatSnackBar
+        private snakBar: MatSnackBar,
+        private router: Router
     ) {
         this.loginForm = this.fb.group({
             email: [null, [Validators.required, Validators.email]],
@@ -33,10 +37,24 @@ export class LoginComponent {
         this.authService.login(this.loginForm.value).subscribe((res) => {
             console.log(res);
             if (res.userId != null) {
-                this.snakBar.open("Login Successfull", "close", {duration: 5000});
+                const user = {
+                    id: res.userId,
+                    role: res.userRole,
+                }
+                StorageService.saveUser(user);
+                StorageService.saveToken(res.jwt);
+
+                if (StorageService.isAdminLoggedIn()) {
+                    this.router.navigateByUrl("/admin/dashboard");
+                } else if (StorageService.isEmployeeLoggedIn()) {
+                    this.router.navigateByUrl("/employee/dashboard");
+                }
+                this.snakBar.open("Login Successfull", "close", { duration: 5000 });
             } else {
-                this.snakBar.open("Invalid credentials", "close", {duration: 5000, panelClass: "error-snackbar"});
+                this.snakBar.open("Invalid credentials", "close", { duration: 5000, panelClass: "error-snackbar" });
             }
         })
     }
+
+
 }
